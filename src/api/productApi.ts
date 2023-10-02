b9690a06cca6c58961b1b2a814be9ae2eeb91c0b
DevRish <rishavchatterjee1546@gmail.com>
2023-10-02 13:56:11 +0530

    added api to fetch single product by id

--- src/api/productApi.ts
+++ src/api/productApi.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getProductData } from "../controller/productController";
+import { getProductById, getProductData } from "../controller/productController";
 
 const router = Router();
 
 router.get("/", getProductData ); // not added isLoggedin as not logged in users should also be able to see products
+router.get("/:id", getProductById ); // not added isLoggedin as not logged in users should also be able to see products
 
 export const productRoutes = router;
--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -11,3 +11,18 @@ export const getProductData = async (req: Request, res: Response) => {
         res.status(500).json({ message: "Server Error" });
     }
 };
+
+export const getProductById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if(product) {
+            res.status(200).json({ message: "Product fetched successfully", product });
+        } else {
+            res.status(404).json({ message: "Product not found" });
+        }
+    } catch (err) {
+        console.log(chalk.redBright(`[-] Error while getting product by id : ${err}`));
+        res.status(500).json({ message: "Server Error" });
+    }
+};
